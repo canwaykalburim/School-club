96b1a8a2b885a36da4be0e92554d2e2646f8693a
10307서동영 <canwaykalburim@gmail.com>
2018-03-30 19:33:39 +0900

    star

---
+++ star/star/pretty.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int n, i, j;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = n; j > i; j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
